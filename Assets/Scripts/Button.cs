223bc76fc1adbe542e555f0a394937ef5d66ee05
afeilke1 <afeilke1@hs-mittweida.de>
2021-01-07 04:24:53 +0100

    Changed Scene Layout
    Added more sprites

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Sprite Clicked");
+        GetComponent<EventTrigger>().GetComponent<IPointerDownHandler>().OnPointerDown(null);
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("Sprite Clicked");
+        GetComponent<EventTrigger>().GetComponent<IPointerDownHandler>().OnPointerDown(null);
+    }
+}
