eebb6f5889e26a19425968b5ab807224041d52f4
afeilke1 <afeilke1@hs-mittweida.de>
2021-01-12 18:43:35 +0100

    Added Scene Fades

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour
+{
+    public int fadeOffset, fadeDuration;
+    public Color fadeColor = new Color(0, 0, 0);
+    public bool start, fadeIn;
+
+    private long tStart;
+    private bool done;
+    private Image img;
+    private Action<BaseEventData> cb = null;
+
+    public enum FadeEvent
+    {
+        NONE, MENU_START_OUT
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = gameObject.GetComponent<Image>();
+        if (start) fadeFrom(gameObject, fadeIn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (done) return;
+
+        float alpha = Mathf.Clamp((time() - tStart) / (float)fadeDuration, 0, 1);
+
+        if (alpha >= 1)
+        {
+            done = true;
+            img.enabled = !fadeIn;
+            if(cb != null) cb(null);
+        }
+        else
+        {
+            if (fadeIn) alpha = 1 - alpha;
+            fadeColor.a = Mathf.Pow(alpha, 3);
+            img.color = fadeColor;
+        }
+    }
+
+    private void doFade(bool fadeIn)
+    {
+        done = false;
+        img.enabled = true;
+        this.fadeIn = fadeIn;
+        tStart = time() + fadeOffset;
+    }
+
+    public void fadeFrom(GameObject obj, bool fadeOut)
+    {
+        cb = null;
+        if (obj)
+        {
+            EventTrigger et = obj.GetComponent<EventTrigger>();
+            if (et) cb = et.OnSubmit;
+        }
+
+        doFade(fadeOut);
+    }
+
+
+    public void FadeOut(BaseEventData ev) { 
+        fadeFrom(((PointerEventData)ev).pointerCurrentRaycast.gameObject, false); 
+    }
+    public void FadeIn(BaseEventData ev) { 
+        fadeFrom(((PointerEventData)ev).pointerCurrentRaycast.gameObject, true); 
+    }
+
+    private static int time() { return (int)(DateTime.Now.Ticks / (int)1e4); }
+}
---
+++ Assets/Scripts/SoundHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void btn_Click()
+    {
+        //playClickSound();
+    }
+}
