f205fe0f59f3397c6ee99a72dc545a7a778844bb
lucas <mhesse4@hs-mittweida.de>
2021-01-20 20:12:49 +0100

    Reset3 working - weird bug where check after reset showing red background - with saving this time
    Signed-off-by: lucas <mhesse4@hs-mittweida.de>

--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -7,17 +7,17 @@ public class Reset : MonoBehaviour
 
     public static void Reset_1()
     {
-    //    foreach (Bowl b in Globals.bowls)
-    //    {
-    //        foreach (Hole h in Globals.getHoles(Hole.LISTHOLE))
-    //        {
-    //            if (b.index == h.value)
-    //             {
-    //                 b.moveToHole(h.GetComponent<Collider2D>());
-    //             }
-    //        }
-    //    }
-    Reset_3();
+       foreach (Bowl b in Globals.bowls)
+       {
+           foreach (Hole h in Globals.getHoles(Hole.LISTHOLE))
+           {
+               if (b.index == h.value)
+                {
+                    b.moveToHole(h.GetComponent<Collider2D>());
+                }
+           }
+       }
+    
     }
 
     public static void Reset_2()
