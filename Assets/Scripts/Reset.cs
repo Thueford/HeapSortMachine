8575d89c19e2aedce35c4dfc279ad4640a9f8cbc
lucas <mhesse4@hs-mittweida.de>
2021-01-20 19:56:20 +0100

    Reset3 working - weird bug where check after reset showing red background
    Signed-off-by: lucas <mhesse4@hs-mittweida.de>

--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -7,27 +7,37 @@ public class Reset : MonoBehaviour
 
     public static void Reset_1()
     {
-       foreach (Bowl b in Globals.bowls)
-       {
-           foreach (Hole h in Globals.getHoles(Hole.LISTHOLE))
-           {
-               if (b.index == h.value)
-                {
-                    Debug.Log("index of bowl "+b.index+"value of hole "+h.value);
-                    b.moveToHole(h.GetComponent<Collider2D>());
-                }
-           }
-       }
+    //    foreach (Bowl b in Globals.bowls)
+    //    {
+    //        foreach (Hole h in Globals.getHoles(Hole.LISTHOLE))
+    //        {
+    //            if (b.index == h.value)
+    //             {
+    //                 b.moveToHole(h.GetComponent<Collider2D>());
+    //             }
+    //        }
+    //    }
+    Reset_3();
     }
 
     public static void Reset_2()
     {
-        // TODO
+        GameObject.Find("edtRuleLeft").GetComponent<InputField>().text = "";
+        GameObject.Find("edtRuleRight").GetComponent<InputField>().text = "";
     }
 
     public static void Reset_3()
     {
-        // TODO
+        foreach (Bowl b in Globals.bowls)
+       {
+           foreach (Hole h in Globals.getHoles(Hole.TREEHOLE))
+           {
+               if (b.index == h.value)
+                {
+                    b.moveToHole(h.GetComponent<Collider2D>());
+                }
+           }
+       }
     }
 
     public static void Reset_4()
